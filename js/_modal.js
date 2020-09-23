981aec113db5f55dc3f502e2f62e30c472fd8d58
sonya-ninja <hello@sonya.ninja>
2020-09-23 17:52:52 +0200

    partner logos with popups. start of subscribe form

---
+++ js/_modal.js
@@ -0,0 +1,26 @@
+var buttons = document.querySelectorAll(".modalBtn");
+var modals = document.querySelectorAll(".modal");
+// var modalContents = document.querySelectorAll(".modalContent");
+
+gsap.set([modals], { autoAlpha: 0, scale: 0 });
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", popUp);
+}
+function popUp() {
+  var parent = this.parentElement;
+  var thisModal = parent.querySelector(".modal");
+  var thisModalContent = thisModal.querySelector(".modalContent");
+  // console.log(modalContent);
+  gsap.to(thisModal, {duration: 0.2, scale:1, autoAlpha:1,
+  });
+}
+for (var i = 0; i < modals.length; i++) {
+  modals[i].addEventListener("click", hide);
+}
+function hide() {
+  var parent = this.parentElement;
+  var thisModal = parent.querySelector(".modal");
+  gsap.to(thisModal, {duration: 0.2, scale:0, autoAlpha:0,
+  });
+}
