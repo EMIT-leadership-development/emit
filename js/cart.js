6869b9c72d28edb914a75e9bc169c33ae2c590d2
sonya-ninja <hello@sonya.ninja>
2021-06-06 06:46:23 +0200

    testing using netlify for crawl verification

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,3 @@
-// CUSTOM DONATION PARAMS
-// Parse the URL parameter
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-// Give the parameter a variable name
-let customContribution = getParameterByName('cc');
-console.log('customContribution is ', customContribution);
-if (customContribution != null) {
-    const button = document.querySelector('#submitCustomDonation');
-    button.setAttribute('data-item-price', customContribution)
-}
-
 // CUSTOM DONATION FORM
 function updateCustomDonation() {
     const button = document.querySelector('#submitCustomDonation');
