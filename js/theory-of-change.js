08df38fc6941dcc964b12e99eae1226f4a5f9a5d
sonya-ninja <hello@sonya.ninja>
2021-10-25 15:47:52 +0200

    joseph section

--- js/theory-of-change.js
+++ js/theory-of-change.js
@@ -4,6 +4,7 @@ window.onload = function() {
 
     introSection();
     eachSection();
+    animateClouds();
 
     function introSection() {
         let tl = gsap.timeline({
@@ -40,7 +41,23 @@ window.onload = function() {
             }
         });
         tl.from(line, {duration:2, drawSVG: 0});
-        tl.from(number,{duration:0.5, autoAlpha:0, scale:0, transformOrigin:"center center", ease:"expo.out"},"<");
+        if (number) {
+            tl.from(number,{duration:0.5, autoAlpha:0, scale:0, transformOrigin:"center center", ease:"expo.out"},"<");
+        }
+    }
+
+    function animateClouds() {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#emit-network',
+                start: "top bottom",
+                end: "bottom top",
+                scrub: 1
+            }
+        });
+        tl.fromTo('#clouds', {rotate:-5, scale:1.3}, {rotate:10, scale:4, transformOrigin:"center center"});
+        tl.fromTo("#backgroundVillage",{scale: 1}, {scale:1.1, transformOrigin:"center center"},"<");
+        tl.fromTo("#joseph",{scale: 0.5, rotate:0}, {scale:1, rotate:2, transformOrigin:"center center"},"<");
     }
 
 }
